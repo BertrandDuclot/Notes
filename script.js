fd7ba9b6fe06e2803d2311558853b11cb296f800
Bertrand D <b.duclot@gmail.com>
2023-11-27 19:10:08 +0100

    Added New note function

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
+const addBtn = document.getElementById("add");
 
+addBtn.addEventListener("click", () => addNewNote());
+
+// Creation of a new note
+function addNewNote(text = "") {
+  const note = document.createElement("div");
+  note.classList.add("note");
+
+  note.innerHTML = `
+    <div class="tools">
+        <button class="edit"><i class="fas fa-edit"></i></button>
+        <button class="delete"><i class="fas fa-trash-alt"></i></button>
+    </div>
+
+    <div class="main ${text ? "" : "hidden"}"></div>
+    <textarea class="${text ? "hidden" : ""}"></textarea>
+    `;
+
+  document.body.appendChild(note);
+}
