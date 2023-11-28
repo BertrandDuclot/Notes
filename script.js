f6dc47b27d48bf77d3d8f9c1ea08954730a8c317
Bertrand D <b.duclot@gmail.com>
2023-11-28 12:25:27 +0100

    Added note addNewNote and updateLocalStorage

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const addBtn = document.getElementById("add");
 
-addBtn.addEventListener("click", () => addNewNote());
+addBtn.addEventListener("click", () => addNewNote("test"));
 
 // Creation of a new note
 function addNewNote(text = "") {
@@ -17,5 +17,38 @@ function addNewNote(text = "") {
     <textarea class="${text ? "hidden" : ""}"></textarea>
     `;
 
+  const editBtn = note.querySelector(".edit");
+  const deleteBtn = note.querySelector(".delete");
+  const main = note.querySelector(".main");
+  const textArea = note.querySelector("textarea");
+
+  textArea.value = text;
+  main.innerHTML = text;
+
+  deleteBtn.addEventListener("click", () => {
+    note.remove();
+  });
+
+  editBtn.addEventListener("click", () => {
+    main.classList.toggle("hidden");
+    textArea.classList.toggle("hidden");
+  });
+
+  // Creation of a variable with the value of what was written
+  textArea.addEventListener("input", (e) => {
+    const { value } = e.target;
+    main.innerHTML = value;
+    updateLocalStorage();
+  });
+
   document.body.appendChild(note);
+}
+
+// Save the notes
+function updateLocalStorage() {
+  const notesText = document.querySelectorAll("textarea");
+  const notes = [];
+  notesText.forEach((note) => notes.push(note.value));
+  console.log(notes);
+  localStorage.setItem("notes", JSON.stringify(notes));
 }
